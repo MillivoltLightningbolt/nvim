25d3d4e826cd0cd1d99e5f32b697377efd6bd3ed
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-01 00:44:25 +0200

    basic packer (plugin-manager) setup

---
+++ lua/plugins.lua
@@ -0,0 +1,24 @@
+-- BOOTSTRAPPING (automatically install packer to any machine this config is cloned to)
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+
+
+
+
+
+-- QUICKSTART
+
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+end)
