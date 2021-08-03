192351dd8dda8b3dad40306f04a5278a0adf2922
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-03 20:39:38 +0200

    Added quick access for various init/config files

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -11,3 +11,16 @@ vim.api.nvim_set_keymap('n', '<S-ENTER>', 'O<ESC>', { noremap = true, silent = t
 
 -- toggle file explorer
 vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<Enter>', { noremap = true, silent = true})
+
+-- press F12 to enter neovims vimrc file
+vim.api.nvim_set_keymap('n', '<F12>', ':e $MYVIMRC<ENTER>', { noremap = true, silent = true})
+
+-- press F11 to enter neovims vimrc file
+vim.api.nvim_set_keymap('n', '<F11>', ':e ~/.config/nvim/lua/keymappings.lua<ENTER>', { noremap = true, silent = true})
+
+-- source: https://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
+-- press F9 to source vimrc file after modifying it
+vim.api.nvim_set_keymap('n', '<F9>', ':source $MYVIMRC<Enter>', { noremap = true, silent = true})
+
+-- press Shift-F9 to source keymappings-file (a.k.a this file) after modifying it
+vim.api.nvim_set_keymap('n', '<F21>', ':source ~/.config/nvim/lua/keymappings.lua<Enter>', { noremap = true, silent = true})
