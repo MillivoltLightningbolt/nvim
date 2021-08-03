fdc1e9fd931882104eaa7e14079a061aa1a3620d
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-03 20:41:50 +0200

    Added moving between windows keybindings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -24,3 +24,10 @@ vim.api.nvim_set_keymap('n', '<F9>', ':source $MYVIMRC<Enter>', { noremap = true
 
 -- press Shift-F9 to source keymappings-file (a.k.a this file) after modifying it
 vim.api.nvim_set_keymap('n', '<F21>', ':source ~/.config/nvim/lua/keymappings.lua<Enter>', { noremap = true, silent = true})
+
+-- move between windows with Alt + movement-keys
+vim.api.nvim_set_keymap('n', '<M-h>', '<C-w>h', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<M-j>', '<C-w>j', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<M-k>', '<C-w>k', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<M-l>', '<C-w>l', { noremap = true, silent = true})
+
