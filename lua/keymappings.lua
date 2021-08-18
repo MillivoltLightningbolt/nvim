251e6a7f7bef319ebb489987b3049389a1771dc7
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-18 14:13:58 +0200

    set up basic keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,41 +1,106 @@
+-- HOWTO use:
+--
+-- vim.api.nvim_set_keymap(
+--    {mode},       -- n: normal, i: interactive, v: visual, c: command-line, t: terminal, ...  => see https://github.com/nanotee/nvim-lua-guide#defining-mappings
+--    {keymap},     -- which combination to press
+--    {mapped to},  -- what the keypress will do
+--    {options}     -- e.g. noremap, silent, expr, ...
+--    )
+
+
+local keymap = vim.api.nvim_set_keymap          -- shortcut
+local opts = { noremap = true, silent = true }  -- commonly used options
+
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+
+
 -- set leader-key to space
-vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
+keymap('n', '<Space>', '<NOP>', opts)
 vim.g.mapleader = ' '
 
--- deactivate highlight-search
-vim.api.nvim_set_keymap('n', '<C-L>', ':nohlsearch<Enter><C-L>', { noremap = true, silent = true})
+-- un-highlight search (will activate on next search)
+keymap('n', '<C-l>', ':nohlsearch<Enter><C-l>', opts)
 
 -- add new line below/above cursor in normal mode with Enter/Shift-Enter
-vim.api.nvim_set_keymap('n', '<ENTER>', 'o<ESC>', { noremap = true, silent = true})	-- below
-vim.api.nvim_set_keymap('n', '<S-ENTER>', 'O<ESC>', { noremap = true, silent = true})	-- above
+keymap('n', '<ENTER>', 'o<ESC>', opts)	  -- below
+keymap('n', '<S-ENTER>', 'O<ESC>', opts)	-- above
 
 -- toggle file explorer
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<Enter>', { noremap = true, silent = true})
+keymap('n', '<Leader>e', ':NvimTreeToggle<Enter>', opts)
 
 -- press F12 to enter neovims vimrc file
-vim.api.nvim_set_keymap('n', '<F12>', ':e $MYVIMRC<ENTER>', { noremap = true, silent = true})
+keymap('n', '<F12>', ':e $MYVIMRC<ENTER>', opts)
 
 -- press F11 to enter neovims vimrc file
-vim.api.nvim_set_keymap('n', '<F11>', ':e ~/.config/nvim/lua/keymappings.lua<ENTER>', { noremap = true, silent = true})
+keymap('n', '<F11>', ':e ~/.config/nvim/lua/keymappings.lua<ENTER>', opts)
 
 -- press Shift-F11 to enter plugins file
-vim.api.nvim_set_keymap('n', '<F23>', ':e ~/.config/nvim/lua/plugins.lua<ENTER>', { noremap = true, silent = true})
+keymap('n', '<F23>', ':e ~/.config/nvim/lua/plugins.lua<ENTER>', opts)
 
 -- source: https://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
 -- press F9 to source vimrc file after modifying it
-vim.api.nvim_set_keymap('n', '<F9>', ':source $MYVIMRC<Enter>', { noremap = true, silent = true})
+keymap('n', '<F9>', ':source $MYVIMRC<Enter>', opts)
 
 -- press Shift-F9 to source keymappings-file (a.k.a this file) after modifying it
-vim.api.nvim_set_keymap('n', '<F21>', ':source ~/.config/nvim/lua/keymappings.lua<Enter>', { noremap = true, silent = true})
+keymap('n', '<F21>', ':source ~/.config/nvim/lua/keymappings.lua<Enter>', opts)
 
 -- move between windows with Alt + movement-keys
-vim.api.nvim_set_keymap('n', '<M-h>', '<C-w>h', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<M-j>', '<C-w>j', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<M-k>', '<C-w>k', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<M-l>', '<C-w>l', { noremap = true, silent = true})
+keymap('n', '<M-h>', '<C-w>h', opts)
+keymap('n', '<M-j>', '<C-w>j', opts)
+keymap('n', '<M-k>', '<C-w>k', opts)
+keymap('n', '<M-l>', '<C-w>l', opts)
 
--- Move current line 1 down in normal mode
-vim.api.nvim_set_keymap('n', '<C-j>', 'ddp', { noremap = true, silent = true})
--- Move current line 1 up in normal mode
-vim.api.nvim_set_keymap('n', '<C-k>', 'ddkP', { noremap = true, silent = true})
+-- Move current line up/down 1 in normal mode
+keymap('n', '<C-j>', 'ddp', opts)   -- down
+keymap('n', '<C-k>', 'ddkP', opts)  -- up
 
+-- autocomplete (nvim-compe plugin) keymapping
+keymap('i', '<C-Space>', 'compe#complete()', { expr = true, noremap = true, silent = true})
+keymap('i', '<ENTER>', 'compe#confirm(\'<Enter>\')', { expr = true, noremap = true, silent = true})
+keymap('i', '<C-e>', 'compe#close(\'<C-e>\')', { expr = true, noremap = true, silent = true})
+keymap('i', '<C-f>', 'compe#scroll({ \'delta\': +4 })', { expr = true, noremap = true, silent = true})
+keymap('i', '<C-d>', 'compe#scroll({ \'delta\': -4 })', { expr = true, noremap = true, silent = true})
+
+
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+--map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-- cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- highlight yanked text (disabled in visual mode
+
+
+
+
+
+
+
+-- local wk = require("which-key")
+
+-- wk.register({
+--   f = {
+--     f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
+--     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false }, -- additional options for creating the keymap
+--     n = { "New File" }, -- just a label. don't create any mapping
+--     e = "Edit File", -- same as above
+--     ["1"] = "which_key_ignore",  -- special label to hide it in the popup
+--     b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
+--   },
+-- }, { prefix = "<leader>" })
