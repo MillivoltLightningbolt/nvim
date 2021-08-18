f31e4ab70f200793a1af5527e8bdc114c5d00c3e
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-18 14:14:05 +0200

    updated plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,10 +16,44 @@ end
 -- SETUP
 return require('packer').startup(function()
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'		-- plugin manager
-  use 'neovim/nvim-lspconfig'		-- configuration for language server
-  use 'kabouzeid/nvim-lspinstall'	-- easy language installation
-  use 'hrsh7th/nvim-compe'		-- autocomplete
-  use 'kyazdani42/nvim-web-devicons'	-- file icons in file explorer
-  use 'kyazdani42/nvim-tree.lua'	-- file explorer
-end)
+  use 'wbthomason/packer.nvim'        -- (this) plugin manager
+  use 'neovim/nvim-lspconfig'         -- configuration for language server
+  use 'hrsh7th/nvim-compe'            -- autocomplete
+  use 'kyazdani42/nvim-web-devicons'  -- file icons in file explorer
+  use 'kyazdani42/nvim-tree.lua'      -- file explorer
+  use 'folke/lua-dev.nvim'            -- lua lsp setup
+  use 'rafamadriz/neon'               -- color scheme
+  use 'savq/melange'                  -- color scheme
+  use 'tpope/vim-commentary'          -- toggle comments
+  use 'tpope/vim-surround'            -- surround stuff with "'{[]}'" etc
+  use 'tpope/vim-repeat'              -- make vim-surround (and some other plugins) repeatable
+  use 'windwp/nvim-autopairs'         -- autopair "'{[( etc
+  use 'tpope/vim-capslock'            -- software capslock
+  use {                               -- treesitter
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'                 -- We recommend updating the parsers on update
+  }
+  use {                               -- telescope
+  'nvim-telescope/telescope.nvim',
+  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'hrsh7th/vim-vsnip'             -- snippets
+  use 'hrsh7th/vim-vsnip-integ'       -- snippets
+  use {                               -- shows options once key is pressed
+    "folke/which-key.nvim",           -- run `:checkhealth which_key` to see any conflicts with which-key
+    config = function()
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+end
+)
+
+
+
+
+
+  -- (NOTE: this was removed from nvim, reason: big and complex): use 'kabouzeid/nvim-lspinstall'	-- easy language installation
