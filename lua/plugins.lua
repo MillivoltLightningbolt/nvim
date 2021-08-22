ece5149d830d7957f32663ab0b3f5997cb99273d
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-08-22 20:55:32 +0200

    Updated Plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,7 +4,8 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local stdpath = fn.stdpath('data') -- ~/.local/share/nvim
+local install_path = stdpath .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
@@ -12,13 +13,17 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 
-
--- SETUP
-return require('packer').startup(function()
+-----------------------------------------------------------
+-- Plugins setup
+-----------------------------------------------------------
+return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'        -- (this) plugin manager
   use 'neovim/nvim-lspconfig'         -- configuration for language server
+
   use 'hrsh7th/nvim-compe'            -- autocomplete
+  -- use 'ms-jpq/coq_nvim'               -- fast autocomplete
+
   use 'kyazdani42/nvim-web-devicons'  -- file icons in file explorer
   use 'kyazdani42/nvim-tree.lua'      -- file explorer
   use 'folke/lua-dev.nvim'            -- lua lsp setup
@@ -39,21 +44,17 @@ return require('packer').startup(function()
   }
   use 'hrsh7th/vim-vsnip'             -- snippets
   use 'hrsh7th/vim-vsnip-integ'       -- snippets
+
   use {                               -- shows options once key is pressed
     "folke/which-key.nvim",           -- run `:checkhealth which_key` to see any conflicts with which-key
     config = function()
       require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
       }
     end
   }
+
 end
 )
 
-
-
-
-
-  -- (NOTE: this was removed from nvim, reason: big and complex): use 'kabouzeid/nvim-lspinstall'	-- easy language installation
+-- NOTE: this was removed from nvim, reason: big and complex:
+-- use 'kabouzeid/nvim-lspinstall'	-- easy language installation
